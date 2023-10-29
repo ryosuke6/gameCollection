1627c1fd13a0c1ee63abca59b87c7bdd2a397c9f
ryosuke6 <89010240+ryosuke6@users.noreply.github.com>
2023-10-29 23:57:39 +0900

    GitHub Pages 公開用の設定

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: process.env.GITHUB_ACTIONS && "/gameCollection",
+    trailingSlash: true,
+    output: "export"
 }
 
 module.exports = nextConfig
